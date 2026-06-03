81a17df2035b5f8f131d517fae5c75fc9273b4fa
yeprepue <yepretel@misena.edu.co>
2026-06-03 11:58:01 -0500

    Services, interfaz, page lista

---
+++ src/app/models/cliente.ts
@@ -0,0 +1,5 @@
+export interface cliente {
+  id: number;
+  nombre: String;
+  image:string
+}
---
+++ src/app/pages/lista-clientes/lista-clientes.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+
+
+@Component({
+  selector: 'app-lista-clientes',
+  templateUrl: './lista-clientes.component.html',
+  styleUrls: ['./lista-clientes.component.css']
+})
+export class ListaClientesComponent {
+
+}
