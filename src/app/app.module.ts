e50c57baa978988c527c33c1edd3c16c312a7b66
yeprepue <yepretel@misena.edu.co>
2026-06-03 11:57:32 -0500

    Estructura inical del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaClientesComponent } from './pages/lista-clientes/lista-clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaClientesComponent
   ],
   imports: [
     BrowserModule,
